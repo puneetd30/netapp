b462772571b24674c6977af05eb2866368659d77
puneet dewan <puneetd30@gmail.com>
2021-09-14 00:49:40 +0800

    Docker configuration

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
 const config ={
-        url : 'mongodb://127.0.0.1:27017/netapp'        
+        url : 'mongodb://mongo:27017/netapp'        
 }
+
+//for local setup
+// const config ={
+//         url : 'mongodb://127.0.0.1:27017/netapp'      
+// }
 
 module.exports=config
