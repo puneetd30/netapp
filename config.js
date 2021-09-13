db841d843f8526be9fe4c4a1ff3765dc012f8568
puneet dewan <puneetd30@gmail.com>
2021-09-14 02:18:32 +0800

    Comment local setup for db

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config ={
         url : 'mongodb://mongo:27017/netapp'        
 }
 
-//for local setup
+// for local setup
 // const config ={
 //         url : 'mongodb://127.0.0.1:27017/netapp'      
 // }
